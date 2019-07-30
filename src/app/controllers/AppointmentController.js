c4f791fdd05c6ed8be18d64227bd612c5c084780
Thauã Borges <thaua97@gmail.com>
2019-07-30 10:32:53 -0300

    Adiciona notificações aos usuarios

--- src/app/controllers/AppointmentController.js
+++ src/app/controllers/AppointmentController.js
@@ -1,9 +1,11 @@
 import * as Yup from 'yup';
-import { startOfHour, parseISO, isBefore } from 'date-fns';
+import { startOfHour, parseISO, isBefore, format } from 'date-fns';
+import pt from 'date-fns/locale/pt';
 
 import Appointment from '../models/Appointment';
 import File from '../models/File';
 import User from '../models/User';
+import Notification from '../schemas/Notification';
 
 class AppointmentController {
     async index(req, res) {
@@ -100,6 +102,21 @@ class AppointmentController {
             user_id: req.userId,
             provider_id,
             data,
+        });
+
+        /**
+         * Notify appointment provider
+         */
+        const user = await User.findByPk(req.userId);
+        const formattedDate = format(
+            hourStart,
+            "'dia' dd 'de' MMMM', às' H'h'mm",
+            { locale: pt }
+        );
+
+        await Notification.create({
+            content: `Novo agendamento de ${user.name} para ${formattedDate}`,
+            user: provider_id,
         });
 
         return res.json(appointment);
